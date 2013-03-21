edf6236622e7a3065f865f485053956d45624bfd
Filip Stenbeck <filip.stenbeck@gmail.com>
2013-03-21 17:22:58 +0100

    clean up

--- war/js/main.js
+++ war/js/main.js
@@ -1,27 +1,29 @@
 function busCtrl($scope, $http) {
-    
-    //Spinner object constructor
+    var target, spinner;
+   
+    //Defining the Spinner object
     $scope.Spinner = function () {
         var opts = {
-    	lines: 13, // The number of lines to draw
-		 length: 7, // The length of each line
-		 width: 4, // The line thickness
-		 radius: 10, // The radius of the inner circle
-		 corners: 1, // Corner roundness (0..1)
-		 rotate: 0, // The rotation offset
-		 color: '#000', // #rgb or #rrggbb
-		 speed: 1, // Rounds per second
-		 trail: 60, // Afterglow percentage
-		 shadow: false, // Whether to render a shadow
-		 hwaccel: false, // Whether to use hardware acceleration
-		 className: 'spinner', // The CSS class to assign to the spinner
-		 zIndex: 2e9, // The z-index (defaults to 2000000000)
-		 top: 'auto', // Top position relative to parent in px
-		 left: 'auto' // Left position relative to parent in px
-	   };
-	   var target = document.getElementById('spinner');
-	   var spinner = new Spinner(opts).spin(target);
-        return spinner;
+            lines: 13,
+            length: 7,
+            width: 4,
+            radius: 10,
+            corners: 1,
+            rotate: 0,
+            color: '#000',
+            speed: 1,
+            trail: 60,
+            shadow: false,
+            hwaccel: false,
+            className: 'spinner',
+            zIndex: 2e9,
+            top: 'auto',
+            left: 'auto'
+        };
+        //Oops, direct DOM manipulation, how rude!
+        target = document.getElementById('spinner');
+        
+        return new Spinner(opts).spin(target);
     };
    
     //First value for bus stop is of cource my stop
@@ -37,7 +39,7 @@ function busCtrl($scope, $http) {
     $scope.spinner = new $scope.Spinner();
     
     //Load data
-    $http.get('minplatsarray?siteId='+$scope.busStop).success(function(data) {
+    $http.get('minplatsarray?siteId=' + $scope.busStop).success(function (data) {
         $scope.buses = data;
         $scope.spinner.stop();
     });
@@ -45,12 +47,10 @@ function busCtrl($scope, $http) {
     //Reload data
     $scope.reload = function reload() {
         $scope.spinner = new $scope.Spinner();
-         $scope.buses = [];
-        $http.get('minplatsarray?siteId='+$scope.busStop).success(function(data) {
-        $scope.buses = data;
-        $scope.spinner.stop();
-	   }); 
-    }
+        $scope.buses = [];
+        $http.get('minplatsarray?siteId=' + $scope.busStop).success(function (data) {
+            $scope.buses = data;
+            $scope.spinner.stop();
+        });
+    };
 }
-
-
